9f3ec573f376a14e133c1ae93ea883e2a90f96e2
Aydın Türker <doktoralban@outlook.com>
2021-01-03 17:01:35 +0300

    Add project files.

---
+++ ConsoleWebApiClient02/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleWebApiClient02
+{
+    public class Product
+    {
+        public int ProductID{get;set;}
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+         
+
+    }
+}
---
+++ ConsoleWebApiClient02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ConsoleWebApiClient02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string url = string.Format("http://localhost:51062/EnUcuz");
+            WebRequest wr = WebRequest.Create(url);
+             wr.Method = "GET";
+            HttpWebResponse res = (HttpWebResponse)wr.GetResponse();
+            using (Stream st = res.GetResponseStream() )
+            {
+                StreamReader sr = new StreamReader(st);
+                string strRes = sr.ReadToEnd();
+                sr.Close();
+                Product pr = (Product)Newtonsoft.Json.JsonConvert.DeserializeObject(strRes,typeof(Product));
+
+                Console.WriteLine(pr.ProductName);
+
+                Console.ReadKey();
+
+              
+            } 
+
+
+
+        }
+
+
+    }
+}
